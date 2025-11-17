8133784b141c28526a3e45af0d66b6936ea857ff
Pablo Daniel Acosta Morales <acostamoralespablodaniel@gmail.com>
2025-11-16 19:31:38 -0600

    Se agrego el boton del mapa

--- client/src/pages/quever/TierraDeVinos.jsx
+++ client/src/pages/quever/TierraDeVinos.jsx
@@ -14,30 +14,36 @@ const TierraDeVinos = () => {
   const navigationItems = [
     //Patrocinadores que pagaron
     { id: 1, 
+      title: 'Mapa de tierra de vinos', 
+      image: 'https://res.cloudinary.com/dbebikryr/image/upload/v1763143254/boton_mapa_vinicolas_du1zt6.png', 
+      externalUrl: "https://view.genially.com/69169ef6f29001777d69cd40/interactive-content-mapa-vinicolas",
+      openInNewTab: true,
+    },
+    { id: 2, 
       title: 'Vinicola el sarmiento', 
       image: 'https://res.cloudinary.com/dbebikryr/image/upload/v1762813225/VinicolaSarmiento_pywyls.png', 
       externalUrl: "https://directoriocomercial.net/vinicola-sarmiento",
       openInNewTab: true,
     },
-    { id: 2, 
+    { id: 3, 
       title: 'Vinicola el aguaje', 
       image: 'https://res.cloudinary.com/dbebikryr/image/upload/v1762820680/VinicolaElAguaje_d68vmv.png', 
       externalUrl: "https://directoriocomercial.net/vinicola-el-aguaje",
       openInNewTab: true, 
     },
-    { id: 3, 
+    { id: 4, 
       title: 'Viñedo las cruces', 
       image: 'https://res.cloudinary.com/dbebikryr/image/upload/v1762820854/VinicolaLasTresCruces_kwaxgg.png', 
       externalUrl: "https://directoriocomercial.net/vinicola-las-cruces",
       openInNewTab: true,
      },
-    { id: 4, 
+    { id: 5, 
       title: 'Hacienda de letras', 
       image: 'https://res.cloudinary.com/dbebikryr/image/upload/v1762820931/VinicolaHaciendaDeLetras_iwn0ii.png', 
       externalUrl: "https://directoriocomercial.net/hacienda-de-letras",
       openInNewTab: true, 
     },
-    { id: 5, 
+    { id: 6, 
       title: 'Segunda parte', 
       image: 'https://res.cloudinary.com/dbebikryr/image/upload/v1763341186/SegundaParte_1_iu0trr.png', 
       externalUrl: "https://directoriocomercial.net/vinicola-segunda-parte",
@@ -45,16 +51,16 @@ const TierraDeVinos = () => {
     },
 
     //Paatrocinadores que no pagaron
-    { id: 6, 
+    { id: 7, 
       title: 'Bodegas origen', 
       image: 'https://res.cloudinary.com/dbebikryr/image/upload/v1762810725/BodegasOrigen_purwum.png', 
       path: '/quever/tierradevinos/bodegasorigen' },
-    { id: 7, title: 'Rancho ubuntu', image: '/assents/imagenes/TierraDeVino/RanchoUbuntu.png', path: '/quever/tierradevinos/ranchoubuntu' },
-    { id: 8, title: 'Real de plata', image: '/assents/imagenes/TierraDeVino/RealDePlata.png', path: '/quever/tierradevinos/realdeplata' },
+    { id: 8, title: 'Rancho ubuntu', image: '/assents/imagenes/TierraDeVino/RanchoUbuntu.png', path: '/quever/tierradevinos/ranchoubuntu' },
+    { id: 9, title: 'Real de plata', image: '/assents/imagenes/TierraDeVino/RealDePlata.png', path: '/quever/tierradevinos/realdeplata' },
     
     
     
-    { id: 9, title: 'Vinicola Santa Elena', image: '/assents/imagenes/TierraDeVino/VinicolaSantaElena.png', path: '/quever/tierradevinos/vinicolasantaelena' },
+    { id: 10, title: 'Vinicola Santa Elena', image: '/assents/imagenes/TierraDeVino/VinicolaSantaElena.png', path: '/quever/tierradevinos/vinicolasantaelena' },
   ];
 
   const handleNavigation = (path) => navigate(path);
